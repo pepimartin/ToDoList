57f420fb192ec3a9b911b9807c3dc837efab8e94
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-05-12 14:48:19 +0200

    CLAU/ checkbox html y JS

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,21 +1,14 @@
-
-const tasksList= document.querySelector("#tasksList);
-
-// 3)
-function showTasks(tasks) {
-    tasks.forEach(task => {
-        const element = `<div class="todo">
-            <p class="list">(${task.todo})</p>
-        </div>`;
-        tasksList.insertAdjacentHTML("beforeend", element)
-    })
+var i=0;
+function add(){
+    if (document.getElementById('test').value!='')
+    {
+        i++;
+        var title = document.getElementById('test').value;
+        var node = document.createElement('div');
+        node.innerHTML = '<input type="checkbox" id="check' + i + '" name="check' + i + '"><label for="check' + i + '">'+ title +'</label>';
+        document.getElementById('container').appendChild(node);
+    }
 }
-// 2)
-let loadTasks = () => {
-    fetch("/tasks")
-        .then(r => r.json())
-        .then(showTasks)
-};
 
 // 1)
 document.addEventListener("DOMContentLoaded", loadTasks)
