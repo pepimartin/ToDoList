41b06716b9e28e043f95f5f74a4b88d3490d8d35
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-05-12 15:11:46 +0200

    delete js

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,14 +1,1 @@
-var i=0;
-function add(){
-    if (document.getElementById('test').value!='')
-    {
-        i++;
-        var title = document.getElementById('test').value;
-        var node = document.createElement('div');
-        node.innerHTML = '<input type="checkbox" id="check' + i + '" name="check' + i + '"><label for="check' + i + '">'+ title +'</label>';
-        document.getElementById('container').appendChild(node);
-    }
-}
 
-// 1)
-document.addEventListener("DOMContentLoaded", loadTasks)
