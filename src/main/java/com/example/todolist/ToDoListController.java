9fe91db1b934d38243cb16e300502dd66f002acf
Pepi Martín <mjmartinmonzonis@hotmail.com>
2021-05-14 11:09:20 +0200

    añadiendo Controller

--- src/main/java/com/example/todolist/ToDoListController.java
+++ src/main/java/com/example/todolist/ToDoListController.java
@@ -6,5 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 @RestController
+public class ToDoListController {
+    private final ArrayList<Task> tasks;
+    public ToDoListController(){
+        tasks = new ArrayList<>();
+    }
+@PostMapping("/tasks")
+public RedirectView addTask(Task task){
+    tasks.add(task);
+    return new RedirectView("/");
+    }
+
 
