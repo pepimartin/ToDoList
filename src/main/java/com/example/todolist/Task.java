2a5cf97d60afdcbf4dc3851799085e25a270d243
mavieuwu <mabigaming26@gmail.com>
2021-05-14 11:44:29 +0200

    Agregar archivo Task.java, cambios en fichas de Java

---
+++ src/main/java/com/example/todolist/Task.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+public class Task {
+    private final String todo;
+
+    public Task(String todo) {
+        this.todo = todo;
+    }
+
+    public String getTask() {
+        return todo;
+    }
+}
+
--- src/main/java/com/example/todolist/ToDoListController.java
+++ src/main/java/com/example/todolist/ToDoListController.java
@@ -1,22 +1,30 @@
-package com.example.todolist.
+package com.example.todolist;
 
 import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 @RestController
 public class ToDoListController {
     private final ArrayList<Task> tasks;
-    public ToDoListController(){
+
+    public ToDoListController() {
         tasks = new ArrayList<>();
     }
-@PostMapping("/tasks")
-public RedirectView addTask(Task task){
-    tasks.add(task);
-    return new RedirectView("/");
+
+    @GetMapping("/tasks")
+    public ArrayList<Task> showTasks() {
+        return tasks;
     }
 
-
+    @PostMapping("/tasks")
+    public RedirectView addTask(Task task) {
+        tasks.add(task);
+        return new RedirectView("/");
+    }
+}
