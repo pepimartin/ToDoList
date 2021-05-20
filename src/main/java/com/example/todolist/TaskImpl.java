0eef1617dab0020d565f38a43e83a242432bde27
superco-art <daniela.trifu@gmail.com>
2021-05-20 13:47:20 +0200

    js

---
+++ src/main/java/com/example/todolist/TaskImpl.java
@@ -0,0 +1,7 @@
+package com.example.todolist;
+
+public class TaskImpl extends Task {
+    public TaskImpl(String tasks) {
+        super(tasks);
+    }
+}
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,9 +1,33 @@
-const taskList = document.getElementById("taskList");
+const taskList = document.querySelector("#taskList");
 
 function addTask(tasks) {
-
-
+    taskList.innerHTML = "";
+    tasks.forEach((task, position) => {
+    const htmlElement = document.createElement("div");
+    htmlElement.innerHTML = `<div class="task">
+            <div class="task__button" 
+            onclick="fetch(\`/tasks/${position}\`, 
+            { method: 'DELETE'}).then(reloadTasks)"></div>
+            <p class="task__label"></p>
+            </div>`;
+        taskList.appendChild(htmlElement)
+    })
 }
+const submitButton = document.getElementById("submit");
+const validateHomework = (event) => {
+    const homework = event.target.value;
+    if (homework.length < 3) {
+        submitButton.disabled = true;
+    } else {
+        submitButton.disabled = false;
+    }
+}
+submitButton.addEventListener("click" , validateHomework);
 
+let reloadTasks = () => {
+    fetch("/")
+        .then (r => r.json())
+        .then(addTask)
+};
 
-
+document.addEventListener("DOMContentLoaded", reloadTasks )
