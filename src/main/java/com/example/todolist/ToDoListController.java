ae72c9944808b82e0dfff4aeddeee073b06f343d
superco-art <daniela.trifu@gmail.com>
2021-05-26 15:14:20 +0200

    cambios en todolistcontroller

--- src/main/java/com/example/todolist/ToDoListController.java
+++ src/main/java/com/example/todolist/ToDoListController.java
@@ -3,21 +3,27 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ToDoListController {
-    private final List<Task> tasks = new ArrayList<>();
+    private Map<Integer,Task> tasks;
+    private Integer counter = 0;
+    public ToDoListController() {
+        tasks = new HashMap<>();
+    }
 
     @GetMapping("/tasks")
-    public List<Task> showTasks() {
+    public Map<Integer, Task> allTasks() {
         return tasks;
     }
 
     @PostMapping("/tasks")
 
     public RedirectView addTask(Task task) {
-        tasks.add(task);
+        tasks.put(counter++, task);
         return new RedirectView("/");
     }
 
